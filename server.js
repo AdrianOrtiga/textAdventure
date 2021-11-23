08af7f590e3fac5fb78f82f75c7f41e5900c1bb8
AdrianOrtiga <adrian.ortiga@gmail.com>
2021-11-23 18:11:37 +0800

    change the database of mongose depending of the env

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ app.set()
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL)
+const dataBase = process.env.NODE_ENV !== 'production' ? process.env.DATABASE_URL : process.env.MONGODB_URI
+mongoose.connect(dataBase)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to the Database succesfuly'))
